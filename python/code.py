aa8cd47a64aedfac4bf17acaceef537558f6b770
zapp <zapp@andnxor.com>
2020-01-10 17:24:24 -0800

    It's alive! HSV to RGB working too.

--- python/code.py
+++ python/code.py
@@ -1,9 +1,10 @@
 import board
 import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
 import time
 
 NUM_LEDS = 5
-pixels = neopixel.NeoPixel(board.APA102_MOSI, NUM_LEDS, auto_write=False, brightness=0.3)
+pixels = neopixel.NeoPixel(board.APA102_MOSI, NUM_LEDS, auto_write=False, brightness=0.1)
 
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -11,17 +12,11 @@ GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
+color1 = fancy.CHSV(0.08, 1.0, 1.0).pack()
 
 while True:
-    pixels.fill(RED)
-    pixels.show()
-
-    # Increase or decrease to change the speed of the solid color change.
-    time.sleep(1)
-    pixels.fill(GREEN)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(BLUE)
-    pixels.show()
-    time.sleep(1)
-
+    for i in range(256):
+        color = fancy.CHSV(i,255,255).pack()
+        pixels.fill(color)
+        pixels.show()
+        time.sleep(0.01)
